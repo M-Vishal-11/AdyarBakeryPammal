d2ed8f23b9b8e9f85594fea26858c42154673990
M-Vishal-11 <m.vishal9a@gmail.com>
2025-06-11 19:55:07 +0530

    updated

--- app/(home)/products/btn.tsx
+++ app/(home)/products/btn.tsx
@@ -9,13 +9,13 @@ const ExpandCollapseBtn = ({ setExpand }: ExpandCollapseBtnProps) => {
     <div className="flex justify-end-safe gap-2">
       <button
         onClick={() => setExpand(false)}
-        className="bg-[#ff1a1a] hover:bg-[#b30000] active:bg-[#b30000] text-white rounded-lg px-1 shadow-md active:shadow-xl"
+        className="bg-[#ff1a1a] hover:bg-[#b30000] active:bg-[#b30000] text-white rounded-lg px-1 shadow-md active:shadow-xl cursor-pointer active:scale-90 transition-transform duration-200"
       >
         Collapse
       </button>
       <button
         onClick={() => setExpand(true)}
-        className="bg-[#ff1a1a] hover:bg-[#b30000] active:bg-[#b30000] text-white rounded-lg px-1 shadow-md active:shadow-xl"
+        className="bg-[#ff1a1a] hover:bg-[#b30000] active:bg-[#b30000] text-white rounded-lg px-1 shadow-md active:shadow-xl cursor-pointer active:scale-110 transition-transform duration-200"
       >
         Expand
       </button>
