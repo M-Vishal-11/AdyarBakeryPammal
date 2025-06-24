b46d180791f4a4e2042b01570bf7dff834eacf9e
M-Vishal-11 <m.vishal9a@gmail.com>
2025-06-24 14:27:30 +0530

    authentication and roles

---
+++ src/middleware.ts
@@ -0,0 +1,37 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isAdminRoute = createRouteMatcher(["/murthy(.*)"]);
+const isOperatorRoute = createRouteMatcher(["/operator(.*)"]);
+
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/",
+  "/cart",
+  "/offers",
+  "/shop",
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    const { sessionClaims } = await auth();
+    const role = sessionClaims?.metadata?.role;
+
+    if (isAdminRoute(req) && role !== "admin") {
+      return NextResponse.redirect(new URL("/404", req.url));
+    }
+    if (isOperatorRoute(req) && !(role === "operator" || role === "admin")) {
+      return NextResponse.redirect(new URL("/404", req.url));
+    }
+
+    await auth.protect();
+  }
+});
+
+export const config = {
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+  ],
+};
